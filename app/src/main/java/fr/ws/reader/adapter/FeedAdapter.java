81df49b7214c1e892118b11e317cad7e37a4a7b1
WANG xin <jeanne_fr@163.com>
2020-01-07 10:17:41 +0100

    feedadapter

--- app/src/main/java/fr/ws/reader/adapter/FeedAdapter.java
+++ app/src/main/java/fr/ws/reader/adapter/FeedAdapter.java
@@ -1,4 +1,37 @@
 package fr.ws.reader.adapter;
 
-public class FeedAdapter {
+import android.support.v7.widget.RecyclerView;
+import java.util.List;
+import fr.ws.reader.base.BaseRecyclerViewAdapter;
+import fr.ws.reader.base.ViewHolderHelper;
+import fr.ws.reader.bean.Feed;
+
+public class FeedAdapter extends BaseRecyclerViewAdapter<Feed> {
+
+    public static final String TAG = "FeedAdapter";
+    private OnFeedOperationListener onFeedOperationListener;
+
+    public FeedAdapter(RecyclerView view, List<Feed> dataList, int layoutId) {
+        super (view, dataList, layoutId);
+    }
+
+    @Override
+    public void convert(ViewHolderHelper helper, Feed feed, int position) {
+        if (feed != null) {
+
+        }
+    }
+
+    public void setOnProductOperationListener(OnFeedOperationListener onFeedOperationListener) {
+        this.onFeedOperationListener = onFeedOperationListener;
+    }
+
+    public interface OnFeedOperationListener {
+
+        void onDetail(Feed feed, int position);
+
+        void addFavorite(Feed feed, int position);
+
+        void deleteFavorite(Feed feed, int position);
+    }
 }
