9872d87f4b44f52630efd9eb19288ce314cc0d04
WANG xin <jeanne_fr@163.com>
2020-01-07 17:56:05 +0100

    recommend

--- app/src/main/java/fr/ws/reader/bean/Feed.java
+++ app/src/main/java/fr/ws/reader/bean/Feed.java
@@ -3,29 +3,77 @@
 package fr.ws.reader.bean;
 
 
-public class Souscription {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Feed {
+
+    public Feed(int entity_id, String title, String link, String description, String image, String categories_string) {
+        this.entity_id = entity_id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.image = image;
+        this.categories_string = categories_string;
+    }
+
+    private int entity_id;
     private String title;
     private String link;
+    private String description;
+    private String image;
+    private String categories_string;
 
-     public Souscription(String title,String link){
-         setLink(link);
-         setTitle(title);
-     }
+    public int getEntity_id() {
+        return entity_id;
+    }
+
+    public void setEntity_id(int entity_id) {
+        this.entity_id = entity_id;
+    }
+
     public String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
-         this.title=title;
-    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getLink() {
         return link;
     }
-    public void setLink(String Link) {
-        this.link=Link;
+
+    public void setLink(String link) {
+        this.link = link;
     }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategories_string() {
+        return categories_string;
+    }
+
+    public void setCategories_string(String categories_string) {
+        this.categories_string = categories_string;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
 
     @Override
@@ -33,24 +81,34 @@ public class Souscription {
         return "Article{" +
                 "title='" + title + '\'' +
                 ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                ", categories_string=" + categories_string +
                 '}';
     }
 
 
-
-    public static final String NAME = Souscription.class.getSimpleName().toLowerCase();
-    public static final String _ID = "_id";
-    public static final String ADDRESS_NAME = "address_name";
-    public static final String ADDRESS = "address";
+    public static final String NAME = Feed.class.getSimpleName().toLowerCase();
+    public static final String _ID = "entity_id";
+    public static final String TITLE = "title";
+    public static final String LINK = "link";
+    public static final String IMAGE = "image";
+    public static final String CATEGORY = "category";
+    public static final String DESCIRPTION = "description";
 
     public static final String CREATE_TABLE =
             "CREATE TABLE " + NAME +
                     " ( " +
                     _ID + " integer primary key autoincrement, " +
-                    ADDRESS + " string unique," +
-                    ADDRESS_NAME + " string " +
+                    TITLE + " string," +
+                    LINK + " string unique," +
+                    IMAGE + " string," +
+                    CATEGORY + " string," +
+                    DESCIRPTION + " string," +
                     " ); ";
 
     public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + NAME;
-    public static String[] Columns = new String[]{_ID,ADDRESS_NAME,ADDRESS};
+    public static String[] Columns = new String[]{_ID,TITLE,LINK,IMAGE,CATEGORY,DESCIRPTION};
+
+
 }
